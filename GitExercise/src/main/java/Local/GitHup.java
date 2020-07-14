d9e308df5ffc0ad8574acf641c2f3b7c1ca0f3fd
sdetbach3 <shirzaimeena1@gmail.com>
2020-07-14 19:59:24 -0400

    this commit

--- GitExercise/src/main/java/Local/GitHup.java
+++ GitExercise/src/main/java/Local/GitHup.java
@@ -3,8 +3,9 @@ package Local;
 public class GitHup {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println( "Helo java");
+		// comment 
+		// this is test class
 	}
 
 }
